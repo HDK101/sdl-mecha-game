a3ce49615a13daed6fdffa9cd90fae6637a98a86
Lucas <lucashideki87@gmail.com>
2021-08-01 16:15:20 -0300

    Started implementing game loop

---
+++ src/game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "sprites.h";
+
+void gameStart(void);
+void gameEvent(SDL_Event *event);
+void gameLoop(void);
+
+#endif
